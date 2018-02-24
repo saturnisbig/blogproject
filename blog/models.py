1bfef216dd8084f7d3b6428c0b94fc3a355f47ec
Teddy <i.kenting@gmail.com>
2018-02-24 23:44:31 +0800

    添加博客应用的模型Category, Tag, Entry

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,33 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    c_time = models.DateTimeField(auto_now_add=True)
+    m_time = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+    c_time = models.DateTimeField(auto_now_add=True)
+    m_time = models.DateTimeField(auto_now=True)
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    # 文章简介，可以为空
+    excerpt = models.CharField(max_length=255, blank=True)
+    body = models.TextField()
+    views = models.IntegerField(default=0)
+    c_time = models.DateTimeField(auto_now_add=True)
+    m_time = models.DateTimeField(auto_now=True)
+    # 外键
+    category = models.ForeignKey(Category)
+    author = models.ForeignKey(User)
+    # 标签，多对多关系，博文可以有多个标签，标签也可以存在于多篇博文
+    tags = models.ManyToManyField(Tag, blank=True)
