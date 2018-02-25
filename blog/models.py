a6064c016e92b7ec082400147edc6e36c210ef39
Teddy <i.kenting@gmail.com>
2018-02-25 16:25:36 +0800

    增加对模型类的说明

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Category(models.Model):
     name = models.CharField(max_length=200)
     c_time = models.DateTimeField(auto_now_add=True)
     m_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=150)
     c_time = models.DateTimeField(auto_now_add=True)
     m_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Entry(models.Model):
     title = models.CharField(max_length=200)
     # 文章简介，可以为空
@@ -31,3 +41,6 @@ class Entry(models.Model):
     author = models.ForeignKey(User)
     # 标签，多对多关系，博文可以有多个标签，标签也可以存在于多篇博文
     tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
