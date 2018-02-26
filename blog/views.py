bafe795cd5bdfe40021b9a116aec667dd0acbac5
Teddy <i.kenting@gmail.com>
2018-02-26 22:29:53 +0800

    添加模板，设置static文件使能够正常显示

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+from blog.models import Entry
+
 
 def index(request):
-    context = {'title': '我的博客首页',
-               'content': '欢迎访问我的博客首页'}
+    post_list = Entry.objects.all().order_by('-created_time')
+    context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
