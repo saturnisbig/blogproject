df20af9d6fe34cb4bd2f3c5afcdffa14407ac477
Teddy <i.kenting@gmail.com>
2018-03-07 23:15:55 +0800

    后台发布文章

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Entry, Category, Tag
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'c_time', 'm_time']
+
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
