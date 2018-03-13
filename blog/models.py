65793a0e1501ffad4f4abb711b68c720a571e672
Teddy <i.kenting@gmail.com>
2018-03-13 23:10:20 +0800

    自动生成文章摘要

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import markdown
 
 from django.shortcuts import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
+from django.utils.html import strip_tags
 
 
 @python_2_unicode_compatible
@@ -43,15 +45,25 @@ class Entry(models.Model):
     # 标签，多对多关系，博文可以有多个标签，标签也可以存在于多篇博文
     tags = models.ManyToManyField(Tag, blank=True)
 
-    def increase_views(self):
-        self.views += 1
-        self.save(update_fields=['views'])
-
-    def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['-c_time']
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        ordering = ['-c_time']
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super(Entry, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
