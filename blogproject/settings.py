a60d3dfb8ef45d8fec861cef92aea78077de9916
Teddy <i.kenting@gmail.com>
2018-03-27 23:17:39 +0800

    STATICFILES_DIR should not contains STATIC_ROOT setting.

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # add static in project dir, where staticfinder will look for static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # collect static file
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
