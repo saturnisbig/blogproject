a58d144fc8ae587a682a6bcc39d5e95e3cabe43b
Teddy <i.kenting@gmail.com>
2018-08-19 11:09:00 +0800

    归档文章计数，模板标签实现

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,7 +16,14 @@ def get_recent_posts(num=5):
 
 @register.simple_tag
 def archive():
-    return Entry.objects.dates('c_time', 'month', order='DESC')
+    archive_list = Entry.objects.dates('c_time', 'month', order='DESC')
+    result = []
+    if archive_list:
+        for date in archive_list:
+            date_list = Entry.objects.filter(c_time__year=date.year,
+                                             c_time__month=date.month)
+            result.append((date, len(date_list)))
+    return result
 
 
 @register.simple_tag
